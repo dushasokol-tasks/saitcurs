76c18ffd9d15158bc69bf2856286a157aa85e7f7
Andrew Sokolov <56156851+dushasokol-tasks@users.noreply.github.com>
2023-06-23 17:20:18 +0300

    Create dann.php

---
+++ dann.php
@@ -0,0 +1,28 @@
+<?php
+session_start();
+//$x = $_POST['poisk'];
+$x = isset($_GET['auto']) $_GET['auto'] ?: $_POST['poisk'];
+if (empty($x)) {
+    require 'index.php';
+} else {
+
+    $db = mysqli_connect('localhost', 'root', '', 'cursachs');
+    $auto = $db->query("select `marka` from `auto` where `marka`='$x' or `markarus`='$x'")->fetch_assoc();
+    if (empty($auto)) {
+        require 'index.php';
+    } else {
+        $y = $db->query("select `markarus` from `auto` where `marka`='$x' or `markarus`='$x'")->fetch_assoc();
+        $z = implode($db->query("select `auto_id` from `auto` where `marka`='$x' or `markarus`='$x'")->fetch_assoc());
+        $m = $db->query("select `type` from `types` where `marka`='$z'")->fetch_all(1);
+        $n = $db->query("select `image` from `types` where `marka`='$z'")->fetch_all(1);
+        $hui = $db->query("select `auto_desk` from `auto` where `marka`='$x' or `markarus`='$x'")->fetch_assoc();
+        $_SESSION['mash'] = implode($auto);
+        $_SESSION['mashrus'] = implode($y);
+        $_SESSION['neskolkom'] = $m;
+        $_SESSION['neskolkon'] = $n;
+        $_SESSION['opisanie'] = implode($hui);
+    }
+    echo "<pre>";
+    var_dump($_SESSION['neskolkon'][0]);
+}
+header('Location: catalog.php');
